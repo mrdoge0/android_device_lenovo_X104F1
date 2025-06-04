32d363e1fa91d10c00fa253d33e4bae812d3d5d5
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-06-04 17:04:31 +0300

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_X104F1.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X104F1-user \
+    lineage_X104F1-userdebug \
+    lineage_X104F1-eng
